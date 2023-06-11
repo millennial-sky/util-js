611c8e8eef78b73230e6b8b30c083184b3f4dcec
millennial-sky <millennial-sky@users.noreply.github.com>
2023-06-11 11:04:30 +0200

    feature: added isString, isNumber and dispatcher

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ export const match = (obj, cases) => {
   }
   return cases[kind](obj)
 }
+
+export const isString = (s) => Object.prototype.toString.call(s) === "[object String]"
+
+export const isNumber = (x) => Object.prototype.toString.call(x) === "[object Number]"
+
+export const dispatcher = () => {
+  const table = []
+
+  const dispatcher = (...args) => {
+    for (let i = table.length - 1; i >= 0; i--) {
+      const [guard, fn] = table[i]
+      if (guard(...args)) return fn(...args)
+    }
+  }
+
+  dispatcher.register = (guard, fn) => {
+    table.push([guard, fn])
+  }
+
+  return dispatcher
+}
